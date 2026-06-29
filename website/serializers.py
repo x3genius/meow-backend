b4fa4957f34a5bb767c1da63f582af0db3ceae29
seraypuh <seraypuh@gmail.com>
2026-06-29 14:31:17 +0300

    image url fix

--- website/serializers.py
+++ website/serializers.py
@@ -2,9 +2,13 @@ from .models import Pet, PetPhoto
 from rest_framework import serializers
 
 class PetPhotoSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = PetPhoto
         fields = '__all__'
+
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None
 
 class PetSerializer(serializers.ModelSerializer):
     age = serializers.ReadOnlyField()
